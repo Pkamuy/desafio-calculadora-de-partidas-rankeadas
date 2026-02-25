aa6e01ea54632a41a539a23ce1438d0b406d5da0
Pkamuy <pedroanderson.cardoso@gmail.com>
2026-02-25 13:49:06 -0300

    second commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+function ranking(qtdVitorias, qtdDerrotas) {
+    let pontuacao = qtdVitorias - qtdDerrotas
+    let classificacao = ""
+
+    switch (true) {
+        case qtdVitorias <= 10:
+            classificacao = "Ferro"
+            break
+
+        case qtdVitorias <= 20:
+            classificacao = "Bronze"
+            break
+
+        case qtdVitorias <= 50:
+            classificacao = "Prata"
+            break
+        
+        case qtdVitorias <= 80:
+            classificacao = "Ouro"
+            break
+
+        case qtdVitorias <= 90:
+            classificacao = "Diamante"
+            break
+
+        case qtdVitorias <= 100:
+            classificacao = "Lendário"
+            break
+
+            default:
+                classificacao ="Imortal"
+    }
+
+    return {pontuacao, classificacao}
+}
+
+let resultado = ranking(85,20)
+
+console.log(
+    `O Herói tem de saldo de ${resultado.pontuacao} está no nível de ${rasultado.classificacao}`
+)
